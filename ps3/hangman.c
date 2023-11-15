70eb2367a1ee32882853be081de544e116e79794
Vladyslav Plotkin <vladyslav.plotkin@student.tuke.sk>
2023-11-15 23:35:55 +0100

    aaa

---
+++ ps3/hangman.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include "hangman.h"
+
+
+int get_word(char secret[]){
+    // check if file exists first and is readable
+    FILE *fp = fopen(WORDLIST_FILENAME, "rb");
+    if( fp == NULL ){
+        fprintf(stderr, "No such file or directory: %s\n", WORDLIST_FILENAME);
+        return 1;
+    }
+
+    // get the filesize first
+    struct stat st;
+    stat(WORDLIST_FILENAME, &st);
+    long int size = st.st_size;
+
+    do{
+        // generate random number between 0 and filesize
+        long int random = (rand() % size) + 1;
+        // seek to the random position of file
+        fseek(fp, random, SEEK_SET);
+        // get next word in row ;)
+        int result = fscanf(fp, "%*s %20s", secret);
+        if( result != EOF )
+            break;
+    }while(1);
+
+    fclose(fp);
+
+    return 0;
+}
