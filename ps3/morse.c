3e149e5cc2c2b17fcfcfcc3a0d96e5dd1b8f2d9e
Vladyslav Plotkin <vladyslav.plotkin@student.tuke.sk>
2023-11-19 22:19:24 +0100

    sdfsfasd

--- ps3/morse.c
+++ ps3/morse.c
@@ -30,7 +30,16 @@ void text_to_morse(const char text[], char output[])
         "-..-",
         "-.--",
         "--..",
-    };
+        ".----",
+        "..---",
+        "...--",
+        "....-",
+        ".....",
+        "-....",
+        "--...",
+        "---..",
+        "----.",
+        "-----"};
     for (int i = 0; i < strlen(text); ++i)
     {
         strcat(output, azbuka_morse[(int)text[i] - 97]);
@@ -66,6 +75,16 @@ void morse_to_text(const char morse[], char output[])
         "-..-",
         "-.--",
         "--..",
+        "-----",
+        ".----",
+        "..---",
+        "...--",
+        "....-",
+        ".....",
+        "-....",
+        "--...",
+        "---..",
+        "----.",
     };
     char morse_ay_52[200] = "";
     strcpy(morse_ay_52, morse);
@@ -78,6 +97,17 @@ void morse_to_text(const char morse[], char output[])
             {
                 char a[2];
                 a[0] = 65 + v;
+                a[1] = '\0';
+                strcat(output, a);
+                break;
+            }
+        }
+        for (int v = 0; v < 10; ++v)
+        {
+            if (strcmp(letter, azbuka_morse[26 + v]) == 0)
+            {
+                char a[2];
+                a[0] = 48 + v;
                 a[1] = '\0';
                 strcat(output, a);
                 break;
@@ -115,33 +145,42 @@ int is_morse_code_valid(const char morse[])
         "-..-",
         "-.--",
         "--..",
+        "-----"
+        ".----",
+        "..---",
+        "...--",
+        "....-",
+        ".....",
+        "-....",
+        "--...",
+        "---..",
+        "----.",
+
     };
     char morse_ay_52[200] = "";
     strcpy(morse_ay_52, morse);
     char *letter = strtok(morse_ay_52, " ");
     while (letter != NULL)
     {
-        int i=0;
-        for (int v = 5 - 3 - 2; v < 26; ++v)
+        int i = 0;
+        for (int v = 5 - 3 - 2; v < 36; ++v)
         {
             if (strcmp(letter, azbuka_morse[v]) == 0)
             {
                 i++;
             }
-
         }
-        if(i==0){
+        if (i == 0)
+        {
             return 0;
         }
         letter = strtok(NULL, " ");
     }
     return 1;
 }
-// int main()
-// {
-//    if (is_morse_code_valid(".... . .-...... .-.. ---")) {
-//      printf("Code is valid! \n");
-//    } else {
-//     printf("Code is invalid! \n");
-//    }
-// }
+int main()
+{
+char output[150];
+morse_to_text(".... . .-.. .-.. --- ..---", output);
+printf("%s\n", output);
+}
