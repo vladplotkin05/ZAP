ab971b06e45b57a121fcbd07a1ac008f68989aa1
Vladyslav Plotkin <vladyslav.plotkin@student.tuke.sk>
2023-11-20 17:04:55 +0100

    modododo

--- ps3/main.c
+++ ps3/main.c
@@ -4,9 +4,11 @@
 #include "hangman.h"
 #include<string.h>
 #include<ctype.h>
+#include<time.h>
 
 int main(){
     char vladcreator[17];
+    srand(time(NULL));
     get_word(vladcreator);
     hangman(vladcreator);
 } 
