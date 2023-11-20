a5a89de296bcbe036033a1da8894a910b6bb9731
Vladyslav Plotkin <vladyslav.plotkin@student.tuke.sk>
2023-11-20 13:36:33 +0100

    2beforemain

--- ps3/main.c
+++ ps3/main.c
@@ -1,4 +1,10 @@
-#include"hangman.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include "hangman.h"
+#include<string.h>
+#include<ctype.h>
+
 int main(){
     char vladcreator[17];
     get_word(vladcreator);
